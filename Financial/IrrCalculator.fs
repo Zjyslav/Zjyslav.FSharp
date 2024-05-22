b6b6bace4e9aaa2d69d1585836ec179b3b185929
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-05-22 18:04:22 +0200

    Initial commit

---
+++ Financial/IrrCalculator.fs
@@ -0,0 +1,21 @@
+﻿namespace Zjyslav.Fsharp.Financial
+
+module IrrCalculator =
+    let internal dcf cashFlow rate period =
+        cashFlow / (pown (1.0 + rate) period)
+
+    let internal npv cashFlows rate =
+        cashFlows
+        |> Array.indexed
+        |> Array.map (fun (i, cf) -> dcf cf rate i)
+        |> Array.sum
+
+    let rec irr cashFlows initialValue increment precision =
+        let presentValue = npv cashFlows initialValue
+
+        if System.Math.Round(presentValue, 2) > 0 then
+            irr cashFlows (initialValue + increment) increment precision
+        else if precision = 0 then
+            initialValue
+        else
+            irr cashFlows (initialValue - increment) (increment / 10.0) (precision - 1)
